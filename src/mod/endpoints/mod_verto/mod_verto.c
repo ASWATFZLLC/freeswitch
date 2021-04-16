5f503b9be623f66496b81685a6dafb7ac4c44db8
Surya Singh <me@suryasingh.xyz>
2021-04-17 00:22:26 +0530

    _ in verto_dial

--- src/mod/endpoints/mod_verto/mod_verto.c
+++ src/mod/endpoints/mod_verto/mod_verto.c
@@ -6277,7 +6277,7 @@ SWITCH_MODULE_LOAD_FUNCTION(mod_verto_load)
 	SWITCH_ADD_API(api_interface, "verto", "Verto API", verto_function, "syntax");
 	SWITCH_ADD_API(api_interface, "verto_contact", "Generate a verto endpoint dialstring", verto_contact_function, "user@domain");
 	SWITCH_ADD_API(api_interface, "verto_pickup", "Request client to pickup the call", verto_pickup_function, "<uuid>");
-	SWITCH_ADD_API(api_interface, "verto dial", "Request client to dial the number", verto_dial_function, "<position_name> <number_to_dial>");
+	SWITCH_ADD_API(api_interface, "verto_dial", "Request client to dial the number", verto_dial_function, "<position_name> <number_to_dial>");
 	switch_console_set_complete("add verto help");
 	switch_console_set_complete("add verto status");
 	switch_console_set_complete("add verto xmlstatus");
