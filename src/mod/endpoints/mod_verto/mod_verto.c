69d5c0b2b15bbad949d56a94c4928761ba061db8
Surya Singh <me@suryasingh.xyz>
2021-04-16 19:55:40 +0530

    test push

--- src/mod/endpoints/mod_verto/mod_verto.c
+++ src/mod/endpoints/mod_verto/mod_verto.c
@@ -5503,6 +5503,20 @@ SWITCH_STANDARD_API(verto_pickup_function)
 	return SWITCH_STATUS_SUCCESS;
 }
 
+SWITCH_STANDARD_API(verto_dial_function)
+{
+	verto_profile_t *profile = NULL;
+	jsock_t *jsock;
+
+		for(profile = verto_globals.profile_head; profile; profile = profile->next) {
+		for (i = 0; i < profile->i; i++) {
+			stream->write_function(stream, "%s\n", profile);
+		}
+	}
+
+	return SWITCH_STATUS_SUCCESS;
+}
+
 
 static switch_call_cause_t verto_outgoing_channel(switch_core_session_t *session,
 												  switch_event_t *var_event,
@@ -6266,6 +6280,7 @@ SWITCH_MODULE_LOAD_FUNCTION(mod_verto_load)
 	SWITCH_ADD_API(api_interface, "verto", "Verto API", verto_function, "syntax");
 	SWITCH_ADD_API(api_interface, "verto_contact", "Generate a verto endpoint dialstring", verto_contact_function, "user@domain");
 	SWITCH_ADD_API(api_interface, "verto_pickup", "Request client to pickup the call", verto_pickup_function, "<uuid>");
+	SWITCH_ADD_API(api_interface, "verto dial", "Request client to dial the number", verto_dial_function, "<position_name> <number_to_dial>");
 	switch_console_set_complete("add verto help");
 	switch_console_set_complete("add verto status");
 	switch_console_set_complete("add verto xmlstatus");
