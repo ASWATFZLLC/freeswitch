c17f93a625d875915c1cf2baa3f0a0129232a475
Surya Singh <me@suryasingh.xyz>
2021-04-17 21:10:52 +0530

    comparison check

--- src/mod/endpoints/mod_verto/mod_verto.c
+++ src/mod/endpoints/mod_verto/mod_verto.c
@@ -5517,9 +5517,9 @@ SWITCH_STANDARD_API(verto_dial_function)
 
 		for (jsock = profile->jsock_head; jsock; jsock = jsock->next) {
 
-			switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "JSOCK %s\n", jsock->uid);
+			switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "JSOCK %s -> %s -> %s -> %s -> %s\n", jsock->uid, zstr(jsock->uid), strcmp(position_name, jsock->uid), zstr(jsock->uid), jsock->ready);
 
-			if (jsock->ready && zstr(jsock->uid) && zstr(position_name) && strcmp(position_name, jsock->uid)) {
+			if (jsock->ready && zstr(jsock->uid) && strcmp(position_name, jsock->uid)) {
 
 				switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "matched  %s -> %s\n", position_name, jsock->uid);
 
