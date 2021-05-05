92b6aceaa17af9f8cfc4d5add0f0261c45b8c81b
Surya Singh <me@suryasingh.xyz>
2021-05-06 00:50:19 +0530

    assign value only if not null

--- src/mod/applications/mod_callcenter/mod_callcenter.c
+++ src/mod/applications/mod_callcenter/mod_callcenter.c
@@ -2797,7 +2797,9 @@ static int members_callback(void *pArg, int argc, char **argv, char **columnName
 				level = atoi(last_agent_tier_level);
 			}
 
-			agent_already_offerd = switch_channel_get_variable(member_channel, "cc_agent_level_offered");
+			if (!switch_channel_get_variable(member_channel, "cc_agent_level_offered")){
+				agent_already_offerd = switch_channel_get_variable(member_channel, "cc_agent_level_offered");
+			}
 
 			switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_WARNING, "surya8523 -> %s \n", agent_already_offerd);
 
