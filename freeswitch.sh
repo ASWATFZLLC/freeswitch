77e94eedbbf25a9261ef5eae06df729e925403ff
SOUFIANE BOUCHAARA <soufiane.devc@gmail.com>
2023-01-12 14:07:37 +0100

    Update freeswitch.sh

--- freeswitch.sh
+++ freeswitch.sh
@@ -3,7 +3,7 @@ set -e
 apt update -y &&  apt upgrade -y && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata
 apt install curl wget git subversion build-essential autoconf automake libtool libncurses5 libncurses5-dev make libjpeg-dev libtool libtool-bin libsqlite3-dev libpcre3-dev libspeexdsp-dev libldns-dev libedit-dev yasm liblua5.2-dev libopus-dev cmake libtiff-dev  libcodec2-dev libcodec2-dev portaudio19-dev libmagickcore-dev libmp3lame-dev libmpg123-dev libshout3-dev libvlc-dev libpq-dev libmariadb-dev libldap2-dev erlang librabbitmq-dev libsmpp34-dev libyaml-dev libmongoc-dev libopencv-dev  libmemcached-dev libavformat-dev  libh2o-dev libsoundtouch-dev libhiredis-dev libopus-dev  autoconf automake devscripts gawk gettext libcurl4-openssl-dev libdb-dev libedit-dev libgdbm-dev libldns-dev libncurses5-dev libopus-dev libopus-ocaml libpcre3-dev libperl-dev libpq-dev libspeex-dev libspeexdsp-dev libssl-dev libtiff5-dev libtool libtool-bin libvorbis0a libogg0 libsqlite3-dev libogg-dev libvorbis-dev portaudio19-dev libshout3-dev libmpg123-dev libmp3lame-dev yasm libbsd-dev flite flite1-dev libflite1 liblua5.2-0 liblua5.2-dev lua5.2 luarocks libsndfile-dev -y
 apt install gcc-10 g++-10 cpp-10 -y && ln -s /usr/bin/gcc-10 /usr/bin/gcc && ln -s /usr/bin/g++-10 /usr/bin/g++
-apt install zip unzip s3cmd
+apt install zip unzip s3cmd -y
 echo "hello" > test.txt 
 s3cmd put test.txt s3://soufiane-test/test.txt --access_key=AKIAX24ALQ6DBLWGLSX3 --secret_key=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
 mkdir -p /opt/openssl && wget https://www.openssl.org/source/openssl-1.1.1q.tar.gz --no-check-certificate -P /opt/openssl/
